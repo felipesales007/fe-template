c3da24fb28e499cd1a711aa322db25faa0b6df9c
Felipe Sales <felipesales007@hotmail.com>
2019-01-30 11:45:20 -0300

    correção de scroll

--- src/app/template/corpo/corpo.component.ts
+++ src/app/template/corpo/corpo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-corpo',
   templateUrl: './corpo.component.html',
@@ -10,6 +12,20 @@ export class CorpoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // para o scroll personalizado
+    var isWindows: boolean;
+
+    (function () {
+      isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+      if (isWindows) {
+        $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
+
+        $('html').addClass('perfect-scrollbar-on');
+      } else {
+        $('html').addClass('perfect-scrollbar-off');
+      }
+    })();
   }
 
 }
